3b068bb77336b9c4c8de5e30f1ad82654e87e3e7
Shane123Reed <shane@cinegrain.com>
2013-11-03 12:25:03 -0800

    First cfBookmarklet commit

---
+++ bookmarklet.js
@@ -0,0 +1,10 @@
+javascript:(function(){
+    var newScript = document.createElement('script');
+    newScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    document.body.appendChild(newScript);
+
+    newScript = document.createElement('script');
+    newScript.src = 'file:///Users/shanereed/wdi/bookmarklet/catchframe.js?dummy='
+                     + Math.random();
+    document.body.appendChild(newScript);
+})();
---
+++ catchframe.js
@@ -0,0 +1,17 @@
+// DOM GETTING ELEMENTS
+// GET IMG
+var images = document.getElementsByTagName('img');
+
+for(var i = 0; i < images.length; i++) {
+  var image = images[i];
+  image.onclick=function(){
+    $.post("http://localhost:3000/bookmarks", { url: this.src } ).done(function( data ) {
+      alert( "Data Loaded: " + data );
+    });
+  };
+}
+
+for(var i = 0; i < images.length; i++) {
+  var image = images[i];
+  image.setAttribute('style', 'border-radius: 20px');
+}
