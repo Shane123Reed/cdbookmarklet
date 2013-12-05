c526797df73083a1e2cf0e4ad89199765e70b1c0
Shane Reed <shane@cinegrain.com>
2013-12-05 10:20:12 -0800

    refactored with jQuery

--- cfbookmarklet.js
+++ cfbookmarklet.js
@@ -1,17 +1,40 @@
 // DOM GETTING ELEMENTS
 // GET IMG
-var images = document.getElementsByTagName('img');
+// var images = document.getElementsByTagName('img');
 
-for(var i = 0; i < images.length; i++) {
-  var image = images[i];
-  image.onclick=function(){
-    $.post("http://localhost:3000/bookmarks", { url: this.src } ).done(function( data ) {
-      alert( "Catchframe: Lookmarked!");
+// for(var i = 0; i < images.length; i++) {
+//   var image = images[i];
+//   image.onclick=function(){
+//     $.post("http://localhost:3000/bookmarks", { url: this.src } ).done(function( data ) {
+//       alert( "Catchframe: Lookmarked!");
+//     });
+//   };
+// }
+
+// for(var i = 0; i < images.length; i++) {
+//   var image = images[i];
+//   image.setAttribute('style', 'border-radius: 20px');
+// }
+
+
+$(function(){
+    //BOOKMARK CLICKED CSS CHANGES
+    $( 'img' ).animate({ borderRadius:'30px' }, 150);
+
+    //IMG CLICKED POST
+    $( 'img' ).click(function() {
+
+        $.post("http://localhost:3000/bookmarks", { url: this.src } ).done(function( data ) {
+            alert( "Catchframe: Lookmarked!");
+        });
+
     });
-  };
-}
+    
 
-for(var i = 0; i < images.length; i++) {
-  var image = images[i];
-  image.setAttribute('style', 'border-radius: 20px');
-}
+
+
+
+
+
+
+});
